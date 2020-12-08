47fa0e44b56a0f3a5fae0be506085d7b979ae121
Thiago Ferrari <thiagofcjr@gmail.com>
2020-12-08 20:20:50 -0300

    install sucrease + preparando debugger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const routes = require('./routes')
+// a partir do node 14 se pode usar o import
+
+import express from 'express'
+import routes from './routes'
 
 class App {
     constructor() {
@@ -18,4 +20,4 @@ class App {
     }
 }
 
-module.exports = new App().server
+export default new App().server
