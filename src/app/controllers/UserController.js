7ed19d16fff4e65181d9b1534f9e32c483cad3d9
thiagoferrari <thiagofcjr@gmail.com>
2020-12-10 10:18:02 -0300

    gerando hash com bcrypt

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,3 +22,5 @@ class UserController {
         })
     }
 }
+
+export default new UserController();
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,6 +3,8 @@
 
 import { Model, Sequelize } from 'sequelize'
 
+//importando bcrypt para usar na senha
+import bcrypt from 'bcryptjs'
 
 
 class User extends Model {
@@ -11,6 +13,10 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+
+                // aqui esse Sequelize.VIRTUAL não é uma coluna no banco, somente é usado para comparar o hash de senha.
+                password: Sequelize.VIRTUAL,
+
                 password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN,
             },
@@ -18,6 +24,18 @@ class User extends Model {
                 sequelize,
             }
         )
+
+        /* esses hooks rastreiam tudo o que acontece no model (semelhante a trigger): */
+        // aqui no beforeSave, vamos fazer isso antes de salvar no banco o dado:
+        this.addHook('beforeSave', async (user) => {
+
+            // se user.password for informado (para criar/editar usuário) então faça isso:
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8)
+            }
+        })
+
+        return this
     }
 }
 
