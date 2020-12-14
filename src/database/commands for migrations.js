4594dfc197a3ce995cf4086493dd6b1c3d890042
thiagoferrari <thiagofcjr@gmail.com>
2020-12-14 14:37:16 -0300

    config MongoDB + mongoose
    Objetivo do MongoDB: armazenar notificações do prestador, quando agendamento é criado.

--- src/database/commands for migrations.js
+++ src/database/commands for migrations.js
@@ -7,3 +7,9 @@ PARA DESFAZER A MIGRATION FEITA:
 yarn sequelize db:migrate:undo
 
 */
+
+/**
+ * COLOCAR CONTAINERS NA MÁQUINA:
+ * docker run --name mongobarber -p 27017:27017 -d -t 
+ * docker run --name database -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres:11
+ */
--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,12 @@
 //Arquivo responsa por conectar o Model na base
 
+import mongoose from 'mongoose'
 import Sequelize from 'sequelize'
 
 // importando models
 import User from '../app/models/User'
 import File from '../app/models/File'
 import Appointment from '../app/models/Appointment'
-
 
 // import responsa por trazer parâmetros do database
 import databaseConfig from '../config/database'
@@ -16,6 +16,7 @@ const models = [User, File, Appointment]
 class Database {
     constructor() {
         this.init()
+        this.mongo()
     }
 
 
@@ -29,6 +30,16 @@ class Database {
         //isso para poder executar (apenas nas classes que contém o método associate - User não tem)
         models.map(model => model.associate && model.associate(this.connection.models))
 
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber', // aqui passo a porta/banco de conex.
+            { useNewUrlParser: true, useFindAndModify: true }
+
+            /* useNewUrlParser serve para usar o novo format URL mongo
+                useFindAndModify: tipo algoritmo de alterações no banco*/
+        )
     }
 
 }
