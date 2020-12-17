409f4cc78e7db2caa2340523a58e7cb882bbf0a2
thiagoferrari <thiagofcjr@gmail.com>
2020-12-17 20:31:42 -0300

    lidando com erros [sentry.io + Youch]
    sentry.io é local que posso ver o erro rastreado, Youch é o framework para apresentar os erros da req. em formato de json (erro que será exibido no front) [qualquer linha errada vai aparecer no json]
    
    LEMBRE-SE:
    yarn dev + yarn queue para fazer o app funfar

--- src/app.js
+++ src/app.js
@@ -2,19 +2,29 @@
 
 import express from 'express'
 import path from 'path'
-import routes from './routes'
+import Youch from 'youch'
+import * as Sentry from '@sentry/node'
+import 'express-async-errors'
+import sentryConfig from './config/sentry'
 
+import routes from './routes'
 import './database'
 
 class App {
     constructor() {
         this.server = express()
 
+        Sentry.init(sentryConfig)
+
         this.middlewares()
         this.routes()
+        this.exceptionHandler()
     }
 
     middlewares() {
+        //aqui se INICIA o rastreamento por erros: (é executado antes de todas rotas)
+        this.server.use(Sentry.Handlers.requestHandler());
+
         this.server.use(express.json())
         /* url para facilitar ao front-end ver o avatar:
                 e aqui vamos usar o express.static para servir arquivos estáticos (png, css, etc.)*/
@@ -23,6 +33,24 @@ class App {
 
     routes() {
         this.server.use(routes)
+
+        //aqui se encerra o rastreamento por erros:
+        this.server.use(Sentry.Handlers.errorHandler())
+    }
+
+
+    /**
+     * FUNÇÃO RESPONSA: retornar msg de erro se algo ser errado
+     */
+    exceptionHandler() {
+        // express entende que middlewares com 4 params é p/ tratar erros:
+        this.server.use(async (err, req, res, next) => {
+
+            // o youch vai retornar os erros [ele irá para o front-end]
+            const errors = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(errors)
+        })
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: "https://41621e40e42f4e508a121a49d5a17ae6@o493113.ingest.sentry.io/5561592",
+}
